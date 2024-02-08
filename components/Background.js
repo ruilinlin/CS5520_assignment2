a9417c77f73dae68c066630d38920bb4fcc61220
ruilinsun <garrolinlin@gmail.com>
2024-02-07 17:32:33 -0800

    initial header componenet color component

---
+++ components/Background.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function background() {
+  return (
+    <View>
+      <Text>background</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
