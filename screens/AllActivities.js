0d8ab0a3960099a846b915788cbfcbb3bfe42614
ruilinsun <garrolinlin@gmail.com>
2024-02-24 10:30:24 -0800

    add getdata function in allactivity screen

--- screens/AllActivities.js
+++ screens/AllActivities.js
@@ -1,32 +1,51 @@
-import { StyleSheet, Text, View, FlatList,Button,SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, FlatList,Button,SafeAreaView,Alert, } from 'react-native'
 import Header from '../components/Header';
 import ActivitiesList from '../components/ActivitiesList';
 import { useRoute } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import colors from '../components/Color';
-import {useActivities} from '../components/ActivitiesContext';
+import { querySnapshot, collection, onSnapshot } from 'firebase/firestore';
+import { database } from "./firebaseSetup";
 
-export default function AllActivities({navigation, route }) {
-//  const [activities, setActivities] = useState([]);
-  const { activities, setActivities } = useActivities();
+export default function AllActivities({navigation}) {
+  const [activities, setActivities] = useState();
+//  const { activities, setActivities } = useActivities();
+
 
   useEffect(() => {
-    if (route.params?.newActivity) {
-      setActivities((currentActivities) => {
+    // Reference to Firestore collection    
+    const collectionRef = collection(database, "activities");
+    onSnapshot(collectionRef,(Snapshot) => {
+      if(Snapshot.empty){
+        Alert.alert("the databse is empty");
+        return;
+      }
+    let updateActivities = [];
+    querySnapshot.array.forEach(doc => {
+      updateActivities.push({...doc.data(),id: doc.id });
+    });
+    setActivities(updateActivities);
+    });
 
-        const doesExist = currentActivities.some(activity =>
-          activity.date === route.params.newActivity.date &&
-          activity.duration === route.params.newActivity.duration &&
-          activity.activity === route.params.newActivity.activity
-        );
+    // if (route.params?.newActivity) {
+    //   setActivities((currentActivities) => {
+
+    //     const doesExist = currentActivities.some(activity =>
+    //       activity.date === route.params.newActivity.date &&
+    //       activity.duration === route.params.newActivity.duration &&
+    //       activity.activity === route.params.newActivity.activity
+    //     );
   
-        if (!doesExist) {
-          return [...currentActivities, route.params.newActivity];
-        }
-        return currentActivities;
-      });
-    }
-  }, [route.params?.newActivity]);
+    //     if (!doesExist) {
+    //       return [...currentActivities, route.params.newActivity];
+    //     }
+    //     return currentActivities;
+    //   });
+    // }
+    
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
   
 
   return (
