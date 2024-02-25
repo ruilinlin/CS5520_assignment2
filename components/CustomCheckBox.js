4bfacfae3f75364e528c7bfccef7f7a1f2b421a5
ruilinsun <garrolinlin@gmail.com>
2024-02-25 14:14:32 -0800

    adjust checkbox outlook style

--- components/CustomCheckBox.js
+++ components/CustomCheckBox.js
@@ -1,13 +1,14 @@
 import CheckBox from 'expo-checkbox';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './Color';
 
 export default function CustomCheckBox({handleremove,text}) {
   const [isChecked, setChecked] = useState(false);
 
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
+      <Text style={styles.text}>{text}</Text>
       <CheckBox 
       style = {styles.checkbox}
       value={isChecked} // Use value to control checked state
@@ -23,10 +24,17 @@ export default function CustomCheckBox({handleremove,text}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width:"93%",
     marginHorizontal: 16,
     marginVertical: 32,
+    flexDirection: 'row',
+  },
+  text:{
+    color:colors.text,
+    marginRight:20,
   },
   checkbox: {
+    marginLeft:30,
     margin: 8,
   },  
 })
